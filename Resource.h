c3fbafb58922ca74143f3f4e3d05990728f00e2c
Martin Payne <development@martinpayne.me.uk>
2017-11-26 21:29:36 +0000

    Moved version info resource into its own file.

--- Resource.h
+++ Resource.h
@@ -3,19 +3,19 @@
 // Used by RESOURCE.RC
 //
 #define IDI_APPICON                     100
-#define IDR_ACCELERATOR                 102
-#define IDD_MAINDIALOG                  103
-#define IDD_ABOUTDIALOG                 104
+#define IDR_ACCELERATOR                 101
+#define IDD_MAINDIALOG                  102
+#define IDD_ABOUTDIALOG                 103
 #define ID_HELP_ABOUT                   200
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
-// 
+//
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 
-#define _APS_NEXT_RESOURCE_VALUE        101
-#define _APS_NEXT_COMMAND_VALUE         101
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         201
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
